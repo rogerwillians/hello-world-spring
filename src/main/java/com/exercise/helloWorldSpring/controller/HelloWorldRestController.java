ba282c19ff0a05c808296411e535429e4c6567ab
Roger Willians <rogerwillians85@gmail.com>
2022-01-27 09:19:45 -0300

    Incluido ResponseBody

--- src/main/java/com/exercise/helloWorldSpring/controller/HelloWorldRestController.java
+++ src/main/java/com/exercise/helloWorldSpring/controller/HelloWorldRestController.java
@@ -2,9 +2,9 @@ package com.exercise.helloWorldSpring.controller;
 
 import com.exercise.helloWorldSpring.HelloWorldRest;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.RequestParam;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
@@ -14,7 +14,8 @@ public class HelloWorldRestController {
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/helloworldrest")
-    public HelloWorldRest helloWorldRest(@RequestParam(value = "nome", defaultValue = "World") String nome) {
+    public @ResponseBody HelloWorldRest helloWorldRest(@RequestParam(value = "nome", defaultValue = "World") String nome)
+    {
         return new HelloWorldRest(counter.incrementAndGet(), String.format(template, nome));
     }
 }
